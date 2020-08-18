1fc72d45c7fcd9b2e4808373b8200dfd36d4ec3d
GetsEezy <69800152+GetsEezy@users.noreply.github.com>
2020-08-18 14:21:34 +0530

    Add files via upload

---
+++ build/precache-manifest.065ebbdb3d016ea62e025c30cab6f6ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1349082895b02f8f02dea4845934df03",
+    "url": "/GetsEezyWeb/index.html"
+  },
+  {
+    "revision": "f5fb68bd076f51f8cb4d",
+    "url": "/GetsEezyWeb/static/css/main.64ec3361.chunk.css"
+  },
+  {
+    "revision": "e056fb6789157050395a",
+    "url": "/GetsEezyWeb/static/js/2.43f08525.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/GetsEezyWeb/static/js/2.43f08525.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5fb68bd076f51f8cb4d",
+    "url": "/GetsEezyWeb/static/js/main.a3c2d1ff.chunk.js"
+  },
+  {
+    "revision": "d65b0d59d363f3836533",
+    "url": "/GetsEezyWeb/static/js/runtime-main.f5e0cefc.js"
+  },
+  {
+    "revision": "57e0e846456f1cc33388d670f1767c03",
+    "url": "/GetsEezyWeb/static/media/5Y.57e0e846.png"
+  },
+  {
+    "revision": "9bea7cb90fcb65312020ad5153128a23",
+    "url": "/GetsEezyWeb/static/media/getseezyLogo.9bea7cb9.png"
+  }
+]);
