5cae72c26f542d4d72e8cbf4c67d012ded39a90e
GetsEezy <69800152+GetsEezy@users.noreply.github.com>
2020-08-18 18:46:19 +0530

    Add files via upload

---
+++ build/precache-manifest.73c09af7e48604473c874d8ec84e8427.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2222125195ca57e8436c62c2c2812cb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6d14f99fabf4732d0ca",
+    "url": "/static/css/main.64ec3361.chunk.css"
+  },
+  {
+    "revision": "e056fb6789157050395a",
+    "url": "/static/js/2.43f08525.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/static/js/2.43f08525.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6d14f99fabf4732d0ca",
+    "url": "/static/js/main.a629df11.chunk.js"
+  },
+  {
+    "revision": "f367d316d91c3181e69f",
+    "url": "/static/js/runtime-main.0364ab79.js"
+  },
+  {
+    "revision": "57e0e846456f1cc33388d670f1767c03",
+    "url": "/static/media/5Y.57e0e846.png"
+  },
+  {
+    "revision": "9bea7cb90fcb65312020ad5153128a23",
+    "url": "/static/media/getseezyLogo.9bea7cb9.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GetsEezyWeb/precache-manifest.065ebbdb3d016ea62e025c30cab6f6ff.js"
+  "/precache-manifest.73c09af7e48604473c874d8ec84e8427.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GetsEezyWeb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
